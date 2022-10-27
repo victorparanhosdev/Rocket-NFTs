d1c579e7b3e03b7767c62abe1ddf57406e49343c
Victor Paranhos <victor_paranhos@hotmail.com>
2022-10-27 00:40:08 -0200

    UP

--- funcao.js
+++ funcao.js
@@ -1,7 +1,16 @@
 const menuHamburguer = document.querySelector('.menu-mobile')
 
-const menuDesktop = document.querySelector('#menu-desktop')
-
+const menuDesktop = document.querySelector('menu')
+valor = true
 menuHamburguer.addEventListener('click', ()=> {
     menuDesktop.classList.toggle('active')
+
+    if(valor) {
+        window.document.body.style = 'overflow: hidden'
+        valor = false
+
+    }else {
+        window.document.body.style = 'overflow: inherit'
+        valor = true 
+    }
 })
